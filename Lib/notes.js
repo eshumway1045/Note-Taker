fd0feeaa3e89fd136b271fafc7b4f1d4c8607d00
Eric Shumway <ericshumway2@mail.weber.edu>
2022-07-06 18:46:47 -0400

    changes made to notes.js, and server.js, as well as setting up routes

--- Lib/notes.js
+++ Lib/notes.js
@@ -11,23 +11,6 @@ function createNewNote (body, notesArray) {
     );
     return note;
 }
-// removes the selected note based on its id 
-function deleteSelectedNote (id, notesArray) {
-    const result = notesArray.filter(note => note.id !== id);
-    console.log(result);
-    fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'),
-        JSON.stringify({ notes: result }, null, 2)
-    );
-};
-// returns the note found with the specific ID
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-};
-// exports functions to be called by server
 module.exports = {
-    createNewNote,
-    deleteSelectedNote,
-    findById
+    createNewNote
 }
